562b16f6441cae78a634c0e8d9429c3f47ec0f06
Rahul Ramkumar <rahulramkumarrrk@gmail.com>
2024-01-18 09:07:06 +0400

    classes part 1

---
+++ Classes Part 1.py
@@ -0,0 +1,34 @@
+class employee:
+    def __init__(self, name, staffno):
+        self.name = name
+        self.staffno = staffno
+        self.fullTimeStaff = True
+
+    def showDetails(self):
+        print(f"Employee name : {self.name}")
+        print(f"Employee number: {self.staffno}")
+    
+class partTime(employee):
+    def __init__(self, name, staffno):
+        super().__init__(name, staffno)
+        self.fullTimeStaff = False
+        self.hoursWorked = 0
+
+    
+    def getHoursWorked(self):
+        return self.hoursWorked
+    
+
+class fullTime(employee):
+    def __init__(self, name, staffno):
+        super().__init__(name, staffno)
+        self.fullTimeStaff = True
+        self.yearlySalary = 0
+
+    def getYearlySalary(self):
+        return self.yearlySalary
+    
+permanentStaff = fullTime("Eric Jones", 72)
+permanentStaff.showDetails()
+temporaryStaff = partTime("Alice Hue", 1017)
+temporaryStaff.showDetails()
