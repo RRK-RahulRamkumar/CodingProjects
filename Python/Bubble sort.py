2375bcf68eb3a3a902e788d6e917ef738735bbe6
Rahul Ramkumar <rahulramkumarrrk@gmail.com>
2024-01-22 10:55:38 +0400

    yes

--- Python/Bubble sort.py
+++ Python/Bubble sort.py
@@ -1,17 +1,23 @@
-# Bubble sort for numbers
-# To bubble sort for words, line 11 list[index][0] > list[index + 1][0]:
 # List must contain the same datatype
 
-list = [1, 3, 2, 4, 7, 6]
+list = [1, 4, 3, 5]
 
 index = 0
+
+def code():
+    global var1, var2, list, index
+    var1, var2 = list[index], list[index + 1]
+    list[index], list[index + 1] = var2, var1
 
 for x in range(len(list)):
     try:
         for i in range(len(list)):
-            if list[index] > list[index + 1]:
-                var1, var2 = list[index], list[index + 1]
-                list[index], list[index + 1] = var2, var1
+            if type(list[0]) == str:
+                if list[index][0] > list[index + 1][0]:
+                    code()
+            elif type(list[0]) == int:
+                if list[index] > list[index + 1]:
+                    code()
             
             index = index + 1
             
