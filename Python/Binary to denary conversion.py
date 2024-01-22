c54b40c13fc9d4181cc60a00373f28a841fda8b0
Rahul Ramkumar <rahulramkumarrrk@gmail.com>
2024-01-22 08:22:36 +0400

    works

--- Python/Binary to denary conversion.py
+++ Python/Binary to denary conversion.py
@@ -1,23 +1,18 @@
-# Taking an input and appending it to user_inputs_store
-user_inputs_store = []
-user_input = input(f"Enter a binary code (1 byte): ")
-user_inputs_store = user_inputs_store + (user_input.split())
+bits_list = []
+user_input = input("Enter binary code (1 Byte): ")
 
-# Convert each string in list to integer
-converted_list = []
-for a_string in user_inputs_store:
-    converted_list.append(int(a_string))
+for bit in user_input:
+    bits_list.append(int(bit))
 
-# Calculating the denary value
-weighting_index = 0
 weighting_list = [128, 64, 32, 16, 8, 4, 2, 1]
+weighting_index = 0
 total = 0
 
-for a_bit in converted_list:
-    if a_bit == 1:
+for bit in bits_list:
+    if bit == 1:
         total = total + weighting_list[weighting_index]
         weighting_index = weighting_index + 1
     else:
         weighting_index = weighting_index + 1
-
+    
 print(f"The denary value is: {total}")
