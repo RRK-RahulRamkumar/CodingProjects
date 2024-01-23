1c5cb976f9e5743d96f80fe1086d5490aa552a31
Rahul Ramkumar <rahulramkumarrrk@gmail.com>
2024-01-23 16:47:07 +0400

    updated

--- Python/Bubble sort.py
+++ Python/Bubble sort.py
@@ -1,17 +1,17 @@
 # List must contain the same datatype
 
+list = []
 
+for x in range(len(list)):
+    index = 0
+    while (index + 1) < len(list):
+        if list[index] > list[index + 1]:
+            var1, var2 = list[index], list[index + 1]
+            list[index], list[index + 1] = var2, var1
+        
+        index = index + 1
 
-
-
-
-
-
-
-
-
-
-
+print(list)
 
 # ______________Old Code______________
 # list = [1, 4, 3, 5]
