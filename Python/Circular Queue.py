9859506f87cc40be72070bd780a4bc995345702d
Rahul Ramkumar <rahulramkumarrrk@gmail.com>
2024-01-24 15:30:05 +0400

    yes

--- Python/Circular Queue.py
+++ Python/Circular Queue.py
@@ -1,4 +1,4 @@
-# Queue follows FIFO structure, use None to declare size of queue
+# Circular Queue follows FIFO structure, use None to declare size of queue
 queue = [None, None, None, None]
 front_pointer = -1
 rear_pointer = -1
@@ -6,11 +6,16 @@ error_message = "Unable to carryout the action."
 def enqueue(enqueue_value):
     global rear_pointer
     rear_pointer = rear_pointer + 1
-    if rear_pointer <= len(queue) - 1:
+    if rear_pointer <= len(queue) - 1 and queue[rear_pointer] == None:
         queue[rear_pointer] = enqueue_value
         print(queue)
     else:
-        print(error_message)
+        rear_pointer = 0
+        if queue[rear_pointer] == None:
+            queue[rear_pointer] = enqueue_value
+            print(queue)
+        else:
+            print(error_message)
 def dequeue():
     global front_pointer
     front_pointer = front_pointer + 1
@@ -18,8 +23,9 @@ def dequeue():
         queue[front_pointer] = None
         print(queue)
     else:
-        print(error_message)
-
+        front_pointer = 0
+        queue[front_pointer] = None
+        print(queue)
 # Extra code
 while True:
     option = int(input("Enqueue(1) or dequeue(2): "))
